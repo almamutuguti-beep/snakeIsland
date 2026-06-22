8a7a45af38e941f2188d25371bf847f27b5f1413
almamutuguti <alma.wanjiru8j@gmail.com>
2026-06-22 21:42:23 +0300

    configure starting snake

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ board_width: int = 20
 # Number of rows in the board
 board_height: int = 15
 
+# Snake starts near the center of the board facing right
+initial_snake_length: int = 3
+
+# Starting head possition(column, row) - middle of the board
+initial_head_x: int = board_width // 2
+initial_head_y: int = board_height // 2
+
+

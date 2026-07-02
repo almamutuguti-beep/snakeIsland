c442a8b53bc51967744914c90c2f46e297e52273
almamutuguti <alma.wanjiru8j@gmail.com>
2026-07-02 08:58:13 +0300

    fix bugs and add get_state() snapshot method in engine file

--- snake_game/food.py
+++ snake_game/food.py
@@ -1,15 +1,9 @@
 from snake_game.models import Point 
-from snake_game.models import self 
-from snake_game.models import random
-
-
-    
-
-
+import random
 
 
 class Food:
-    def __init__(self, grid_width, grid_height, cell_size=20):
+    def __init__(self, grid_width: int, grid_height: int, cell_size: int=20):
         self.grid_width = grid_width
         self.grid_height = grid_height
         self.position = None
@@ -23,30 +17,30 @@ class Food:
             self.position = random.choice(free_cells)
             return self.position
         
-        def get_position(self):
-            return self.position
+    def get_position(self):
+        return self.position
+    
+    def generate_random_position(self):
+        x = random.randint(0, self.grid_width -1)
+        y = random.randint(0, self.grid_height -1)
+        return Point(x,y)
+    
+    def set_position(self, new_position: Point):
+        self.position = new_position
+
+    def get_cell_size(self):
+        return self.cell_size
+    
+    def set_cell_size(self, new_cell_size):
+        self.cell_size = new_cell_size
+
+    def get_grid_dimensions(self):
+        return self.grid_width, self.grid_height
+    
+    def set_grid_dimensions(self, new_width, new_height):
+        self.grid_width = new_width
+        self.grid_height = new_height
+
         
-        def generate_random_position(self):
-            x = random.randint(0, self.grid_width -1)
-            y = random.randint(0, self.grid_height -1)
-            return Point(x,y)
-        
-        def set_position(self, new_position: Point):
-            self.position = new_position
-
-            def get_cell_size(self):
-                return self.cell_size
-            
-            def set_cell_size(self, new_cell_size):
-                self.cell_size = new_cell_size
-
-                def get_grid_dimensions(self):
-                    return self.grid_width, self.grid_height
-                
-                def set_grid_dimensions(self, new_width, new_height):
-                    self.grid_width = new_width
-                    self.grid_height = new_height
-
-                    
 
 
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -2,79 +2,80 @@ from turtle import position
 from models import Point, Direction
 
 
-class snake:
+class Snake:
     def __init__(self, start_pos: Point, cell_size=20):
         self.body = [start_pos]
         self.cell_size = cell_size
         self.direction = Direction.RIGHT # intial direction is right
         self.grow_pending = False
 
-        def change_direction(self, new_direction):
-            if (new_direction[0] * -1, new_direction[1] * -1) != self.direction:
-                self.direction = new_direction 
+    def change_direction(self, new_direction: Direction) -> None:
+        """Reject the new direction if it is opposite to the current direction"""
+        if not new_direction.is_opposite(self.direction):
+            self.direction = new_direction
+
+    def move(self) -> None:
+        head = self.body[0]
+        new_head = head + self.direction.value
+        self.body.insert(0, new_head)
+        if not self.grow_pending:
+            self.body.pop()
+        else:
+            self.grow_pending = False
+
+    def grow(self):
+        self.grow_pending = True
+
+    def get_head_position(self) -> Point:
+        return self.body[0]
                 
-                def move(self):
-                    head = self.body[0]
-                    new_head = head + self.direction.value
-                    self.body.insert(0, new_head)
-                    if not self.grow_pending:
-                        self.body.pop()
-                    else:
-                        self.grow_pending = False
+    def get_body_positions(self):
+        return self.body
+                
+    def check_collision(self, position : Point)-> bool:
+        return position in self.body
+                
+    def check_self_collision(self) -> bool:
+        head_position = self.get_head_position()
+        return head_position in self.body[1:]
+                
+    def reset(self, start_pos: Point):
+        self.body = [start_pos]
+        self.direction =  Direction.RIGHT # reset direction to right
 
-                        def grow(self):
-                            self.grow_pending = True
+    def get_direction(self)-> Direction:
+        return self.direction
+    
+    def set_direction(self, new_direction):
+        self.direction = new_direction
 
-                            def get_head_position(self) -> Point:
-                                return self.body[0]
-                            
-                            def get_body_positions(self):
-                                return self.body
-                            
-                            def check_collision(self, position : Point)-> bool:
-                                return position in self.body
-                            
-                            def check_self_collision(self) -> bool:
-                                head_position = self.get_head_position()
-                                return head_position in self.body[1:]
-                            
-                            def reset(self, start_pos: Point):
-                                self.body = [start_pos]
-                                self.direction =  Direction.RIGHT # reset direction to right
+    def get_cell_size(self):
+        return self.cell_size
 
-                                def get_direction(self)-> Direction:
-                                    return self.direction
-                            
-                            def set_direction(self, new_direction):
-                                self.direction = new_direction
+    def set_cell_size(self, new_cell_size):
+        self.cell_size = new_cell_size
 
-                                def get_cell_size(self):
-                                    return self.cell_size
+    def get_length(self):
+        return len(self.body)
+                
+    def set_length(self, new_length):
+        if new_length < len(self.body):
+            self.body = self.body[:new_length]
 
-                                    def set_cell_size(self, new_cell_size):
-                                        self.cell_size = new_cell_size
+    def get_body(self):
+        return self.body
+                        
+    def set_body(self, new_body):
+        self.body = new_body
 
-                                        def get_length(self):
-                                            return len(self.body)
-                                        
-                                        def set_length(self, new_length):
-                                            if new_length < len(self.body):
-                                                self.body = self.body[:new_length]
+    def get_grow_pending(self):
+        return self.grow_pending
+    
+    def set_grow_pending(self, new_grow_pending):
+        self.grow_pending = new_grow_pending
 
-                                                def get_body(self):
-                                                    return self.body
-                                                
-                                                def set_body(self, new_body):
-                                                    self.body = new_body
-
-                                                    def get_grow_pending(self):
-                                                        return self.grow_pending
-                                                    
-                                                    def set_grow_pending(self, new_grow_pending):
-                                                        self.grow_pending = new_grow_pending
-
-                                                        def get_head(self):
-                                                            return self.body[0]
-                                                        
-                                                        def set_head(self, new_head):
-                                                            self.body[0] = new_head
+    def get_head(self):
+        return self.body[0]
+    
+    def set_head(self, new_head):
+        self.body[0] = new_head

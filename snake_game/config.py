0770b1d5c4d578d2b125bdd368d5fa4acbd29363
almamutuguti <alma.wanjiru8j@gmail.com>
2026-07-02 09:04:09 +0300

    align initial_snake_length with actual spawn behavior

--- snake_game/config.py
+++ snake_game/config.py
@@ -7,7 +7,7 @@ board_width: int = 20
 board_height: int = 15
 
 # Snake starts near the center of the board facing right
-initial_snake_length: int = 3
+initial_snake_length: int = 1
 
 # Starting head possition(column, row) - middle of the board
 initial_head_x: int = board_width // 2
